a1184e85d061a42d987377a4b29128e28f70a1f2
@esouza <esouza@palo-it.com>
2025-08-02 21:02:59 -0300

    adding database and migrations

--- cmd/ordersystem/main.go
+++ cmd/ordersystem/main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 
 	"github.com/EuricoCruz/cleanarch_challenge/configs"
 	"github.com/EuricoCruz/cleanarch_challenge/internal/infra/web"
+	
+	// mysql
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	configs, err := configs.LoadConfig("./"); if err != nil {
 		panic(err)
 	}
+
+	db, err := sql.Open(configs.DBDriver, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", configs.DBUser, configs.DBPassword, configs.DBHost, configs.DBPort, configs.DBName))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
 	webserver := webserver.NewWebServer(configs.WebServerPort)
 	fmt.Println("Starting web server on port", configs.WebServerPort)
